062237e53e63383e14a37750bc500a270f18fc52
davay <fond@fondfrukt.ru>
2021-11-27 15:27:29 +0300

    gh

--- src/components/nav/bar.vue
+++ src/components/nav/bar.vue
@@ -3,7 +3,6 @@ const links = {
   '/': 'Home',
   '/players': 'Players'
 }
-
 </script>
 
 <template lang="pug">
@@ -11,7 +10,9 @@ const links = {
   router-link.p-2(v-for="(link,l) in links" :key="link" 
   :to="l" 
   :class="{ active: $route.path == l }") {{ link }}
-
+  .flex-1
+  a.text-2xl.m-2(href="https://github.com/davay42/vite-gun-starter" target="_blank")
+    la-github
 </template>
 
 <style scoped>
