f6787397478948d2f9c6f7c5b5347dea153145f6
davay42 <fond@fondfrukt.ru>
2021-12-29 12:20:50 +0300

    jsdoc

--- composables/use/account.js
+++ composables/use/account.js
@@ -3,37 +3,53 @@
  * @module Account
  * */
 
-import { gun } from "./gun";
-import { color } from "./color";
+/**
+ * @typedef {Object} Account - the user account interface
+ * @property {Ref} pub - The pub key used to build the account
+ * @property {Computed} color - The user account color derived from the pub key
+ * @property {Object} profile - An object with all the `gun.user().get('profile')` data
+ * @property {Number} pulse - latest timestamp from the user
+ * @property {Boolean} blink - A boolean that toggles on every timestamp received
+ * @property {Gun ref} db - `gun.user(pub)` ref to query any additional user data
+ */
+
+import { gun } from './gun'
+import { color } from './color'
+
+/**
+ * A user's account
+ * @param {Ref} pub - reactive public key in a ref
+ * @returns {Account}
+ */
 
 export function useAccount(pub = ref()) {
   const account = computed(() => {
     const obj = reactive({
       pub,
-      color: computed(() => (pub.value ? color.deep.hex(pub.value) : "gray")),
+      color: computed(() => (pub.value ? color.deep.hex(pub.value) : 'gray')),
       profile: {
-        name: "",
+        name: '',
       },
       pulse: 0,
       blink: false,
       db: gun.user(pub.value),
-    });
+    })
 
     gun
       .user(pub.value)
-      .get("pulse")
+      .get('pulse')
       .on((d) => {
-        obj.blink = !obj.blink;
-        obj.pulse = d;
+        obj.blink = !obj.blink
+        obj.pulse = d
       })
       .back()
-      .get("profile")
+      .get('profile')
       .map()
       .on((data, key) => {
-        obj.profile[key] = data;
-      });
-    return obj;
-  });
+        obj.profile[key] = data
+      })
+    return obj
+  })
 
-  return { account };
+  return { account }
 }
