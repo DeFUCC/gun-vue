0f9a4da4591dfd2aa0776e31b918f761dce67112
Ali Zemani <mehot1@gmail.com>
2022-01-18 10:29:13 +0330

    fix Uppercase folder ERR

--- components/src/index.js
+++ components/src/index.js
@@ -2,49 +2,49 @@ import "virtual:windi.css";
 import "./styles/index.css";
 import "./styles/transitions.css";
 
-export { default as AccountProfile } from "./account/Profile.vue";
-export { default as AccountAvatar } from "./account/Avatar.vue";
-export { default as AccountBadge } from "./account/Badge.vue";
-export { default as AccountHome } from "./account/Home.vue";
-export { default as AccountMate } from "./account/Mate.vue";
-export { default as AccountMates } from "./account/Mates.vue";
-export { default as AccountStars } from "./account/Stars.vue";
+export { default as AccountProfile } from "./account/profile.vue";
+export { default as AccountAvatar } from "./account/avatar.vue";
+export { default as AccountBadge } from "./account/badge.vue";
+export { default as AccountHome } from "./account/home.vue";
+export { default as AccountMate } from "./account/mate.vue";
+export { default as AccountMates } from "./account/mates.vue";
+export { default as AccountStars } from "./account/stars.vue";
 
-export { default as UserCredentials } from "./user/Credentials.vue";
-export { default as UserPassphrase } from "./user/Pass.vue";
-export { default as UserAuth } from "./user/Auth.vue";
-export { default as UserLogin } from "./user/Login.vue";
-export { default as UserHome } from "./user/Home.vue";
-export { default as UserIcon } from "./user/Icon.vue";
-export { default as UserPanel } from "./user/Panel.vue";
-export { default as UserProfile } from "./user/Profile.vue";
-export { default as UserProfileField } from "./user/Profile/Field.vue";
+export { default as UserCredentials } from "./user/credentials.vue";
+export { default as UserPassphrase } from "./user/pass.vue";
+export { default as UserAuth } from "./user/auth.vue";
+export { default as UserLogin } from "./user/login.vue";
+export { default as UserHome } from "./user/home.vue";
+export { default as UserIcon } from "./user/icon.vue";
+export { default as UserPanel } from "./user/panel.vue";
+export { default as UserProfile } from "./user/profile.vue";
+export { default as UserProfileField } from "./user/profile/field.vue";
 
-export { default as RelayIcon } from "./relay/Icon.vue";
+export { default as RelayIcon } from "./relay/icon.vue";
 
-export { default as RoomProfile } from "./room/Profile.vue";
+export { default as RoomProfile } from "./room/profile.vue";
 
-export { default as SpacePlane } from "./space/Plane.vue";
-export { default as SpaceGuest } from "./space/Guest.vue";
+export { default as SpacePlane } from "./space/plane.vue";
+export { default as SpaceGuest } from "./space/guest.vue";
 
-export { default as FeedList } from "./feed/List.vue";
-export { default as FeedLabel } from "./feed/Label.vue";
-export { default as FeedBlock } from "./feed/Block.vue";
+export { default as FeedList } from "./feed/list.vue";
+export { default as FeedLabel } from "./feed/label.vue";
+export { default as FeedBlock } from "./feed/block.vue";
 
-export { default as PostCard } from "./post/Card.vue";
-export { default as PostForm } from "./post/Form.vue";
-export { default as PostList } from "./post/List.vue";
-export { default as PostPage } from "./post/Page.vue";
-export { default as PostStar } from "./post/Star.vue";
+export { default as PostCard } from "./post/card.vue";
+export { default as PostForm } from "./post/form.vue";
+export { default as PostList } from "./post/list.vue";
+export { default as PostPage } from "./post/page.vue";
+export { default as PostStar } from "./post/star.vue";
 
-export { default as LogTree } from "./log/Tree.vue";
+export { default as LogTree } from "./log/tree.vue";
 
-export { default as QrShow } from "./qr/Show.vue";
-export { default as QrLoad } from "./qr/Load.vue";
+export { default as QrShow } from "./qr/show.vue";
+export { default as QrLoad } from "./qr/load.vue";
 
-export { default as EmbedYoutube } from "./embed/Youtube.vue";
+export { default as EmbedYoutube } from "./embed/youtube.vue";
 
-export { default as UiModal } from "./ui/Modal.vue";
+export { default as UiModal } from "./ui/modal.vue";
 
 export { default as UtilPulse } from "./util/pulse.vue";
 export { default as UtilGraph } from "./util/graph.vue";
