c323a32d75202168f5bd0fd74cea162611cc544d
davay <fond@fondfrukt.ru>
2022-01-22 21:08:37 +0300

    m2

--- demo/src/pages/index.vue
+++ demo/src/pages/index.vue
@@ -11,16 +11,16 @@
       .my-2 All the data in the app is stored only on the client side - right in your browser. And it's reactively synced between all simultaneous visitors by Gun. So it's better to share a link to this page with your friend to have a more profound demo experience. 
       .my-4 We're building basic components and data structures as a foundation for various Vue 3 apps to built by anyone. Feel free to play with it and to make any contributions! 
       .flex.flex-wrap
-        a.button.m-1.flex.items-center(href="/docs/" target="_blank")
+        a.button.m-2.flex.items-center(href="/docs/" target="_blank")
           la-book-open.mr-4.text-3xl
           .text-xl Read docs
-        a.button.m-1.flex.items-center(href="https://www.npmjs.com/org/gun-vue" target="_blank")
+        a.button.m-2.flex.items-center(href="https://www.npmjs.com/org/gun-vue" target="_blank")
           la-npm.mr-4.text-3xl
           .text-xl Install packages
-        a.button.m-1.flex.items-center(href="https://github.com/defucc/gun-vue" target="_blank")
+        a.button.m-2.flex.items-center(href="https://github.com/defucc/gun-vue" target="_blank")
           la-github.mr-4.text-3xl
           .text-xl Contribute code
-        a.button.m-1.flex.items-center(href="https://ko-fi.com/davay" target="_blank")
+        a.button.m-2.flex.items-center(href="https://ko-fi.com/davay" target="_blank")
           cib-ko-fi.mr-4.text-3xl
           .text-xl Support me
 
