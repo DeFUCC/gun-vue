b339bcc8570ddc13d79893baff0a30117d28801d
davay <fond@fondfrukt.ru>
2022-01-23 12:35:03 +0300

    no zenbase + qrcode in terminal

--- relay/server.js
+++ relay/server.js
@@ -1,6 +1,5 @@
 import express from "express";
 import Gun from "gun";
-// import "zenbase";
 import qr from "qrcode-terminal";
 import ip from "ip";
 
@@ -19,10 +18,6 @@ export default {
       file: "store",
       radisk: store,
       web: server,
-      // secret: "gun-vue-demo",
-      // portal: "https://siasky.net",
-      // debug: false,
-      // until: 2 * 1000,
     });
     app.use(express.static(path));
     const db = gun.get(host);
