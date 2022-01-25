0905424f033da52d62fc7932b3e9ff06306d6665
davay <fond@fondfrukt.ru>
2022-01-26 00:57:56 +0300

    SW to be added

--- demo/vite.config.js
+++ demo/vite.config.js
@@ -2,15 +2,18 @@ import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import Components from "unplugin-vue-components/vite";
 import Pages from "vite-plugin-pages";
+
+import { VitePWA } from "vite-plugin-pwa";
+
+import Icons from "unplugin-icons/vite";
+import IconsResolver from "unplugin-icons/resolver";
+import WindiCSS from "vite-plugin-windicss";
+
 import path from "node:path";
 import { fileURLToPath } from "node:url";
 
 const filename = fileURLToPath(import.meta.url);
 const dirname = path.dirname(filename);
-
-import Icons from "unplugin-icons/vite";
-import IconsResolver from "unplugin-icons/resolver";
-import WindiCSS from "vite-plugin-windicss";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -47,6 +50,41 @@ export default defineConfig({
         }),
       ],
     }),
+    // VitePWA({
+    //   manifest: {
+    //     name: "Gun-Vue",
+    //     short_name: "@gun-vue",
+    //     description: "Peer-to-peer graph database UI components",
+    //     iconPath: "./media/gun-vue-logo.png",
+    //     theme_color: "#ff0088",
+    //     display: "standalone",
+    //     homepage_url: "https://gun-vue.js.org",
+    //     orientation: "portrait",
+    //     providedBy: {
+    //       name: "defucc",
+    //       url: "https://defucc.me",
+    //     },
+    //     icons: [
+    //       {
+    //         src: "./media/gun-vue-logo.svg",
+    //         type: "image/svg+xml",
+    //         sizes: "512x512",
+    //       },
+    //       {
+    //         src: "./media/gun-vue-logo.png",
+    //         sizes: "192x192",
+    //         type: "image/png",
+    //         purpose: "any maskable",
+    //       },
+    //       {
+    //         src: "./media/gun-vue-logo.png",
+    //         sizes: "512x512",
+    //         type: "image/png",
+    //         purpose: "any maskable",
+    //       },
+    //     ],
+    //   },
+    // }),
   ],
   build: {
     outDir: "../_dist/",
