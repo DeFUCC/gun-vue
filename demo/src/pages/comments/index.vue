ad1de34d7863a05eb5f46765fb15cd32a2c98b67
davay <fond@fondfrukt.ru>
2022-01-27 17:13:24 +0300

    no comments

--- demo/src/pages/comments/index.vue
+++
@@ -1,7 +0,0 @@
-<script setup>
-</script>
-
-<template lang='pug'>
-.p-4 
-  post-feed-block(host="OVEIlbwn0NHhyD4S14t6JlIKauKj9Fl-lfqIcJLynPM.VCBFfxMJfZNrIXhj9s3m4QOZbvl7hljrwnG-GQGQZA0")
-</template>
--- demo/src/pages/routes.js
+++ demo/src/pages/routes.js
@@ -2,5 +2,4 @@ export const routes = {
   "/spaces/": "Spaces",
   "/feeds/": "Feeds",
   "/users/": "Users",
-  "/comments/": "Comments",
 };
