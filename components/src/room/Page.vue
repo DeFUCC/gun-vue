09a990a1a6ca6364392c1960e846af95f665a523
davay <fond@fondfrukt.ru>
2022-02-11 15:54:26 +0300

    fix

--- components/src/room/Page.vue
+++ components/src/room/Page.vue
@@ -40,12 +40,17 @@ const bg = computed(() => useBackground(roomPub.value, 600))
             :editable="room.hosts[user.pub] && roomPub == currentRoom.pub && !editName"
             @update="updateRoomProfile('name', $event)"
           )
-          .flex.items-center
+          .flex.items-center.flex-wrap
             .font-bold.mr-2 Hosts: 
             .p-2(v-for="(enc, host) in room.hosts" :key="host")
               account-badge( :pub="host" :selectable="true")
               room-features(:features="enc") Tools
         .flex-1
+      room-features.my-4(:features="room.features")
+      .flex.flex-wrap
+        button.button(v-if="room.hosts?.[user.pub]?.enc" @click="recreateRoom(room.hosts?.[user.pub]?.enc)")
+          la-tools
+          .ml-2 Renew
         .flex.flex-wrap.py-4(v-if="roomPub != rootRoom.pub")
           button.button(@click="enterRoom(roomPub)" v-if="currentRoom.pub !== roomPub")
             ion-enter-outline
@@ -53,11 +58,6 @@ const bg = computed(() => useBackground(roomPub.value, 600))
           button.button(@click="leaveRoom()" v-else)
             ion-exit-outline
             .ml-2 Leave
-      room-features.my-4(:features="room.features")
-      .flex.flex-wrap
-        button.button(v-if="room.hosts?.[user.pub]?.enc" @click="recreateRoom(room.hosts?.[user.pub]?.enc)")
-          la-tools
-          .ml-2 Renew
   .text-center.flex.flex-col.items-center
     .m-2.p-4.shadow-lg.rounded-2xl
       .text-sm.font-mono {{ room.profile }}
