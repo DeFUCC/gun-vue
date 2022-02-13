c0a5dc4fca7044627aada2230a4954b6abc545c0
davay <fond@fondfrukt.ru>
2022-02-13 14:59:52 +0300

    chat channels

--- components/src/chat/Room.vue
+++ components/src/chat/Room.vue
@@ -1,29 +1,61 @@
 <script setup>
+import { ref, onMounted, watch, nextTick } from 'vue'
+import { useMediaQuery, onClickOutside } from '@vueuse/core'
 import { useChat, useUser } from '@composables';
 
 const { user } = useUser();
 
-const { send, message, messages, chatWindow } = useChat()
+const { send, currentTopic, topics, messages } = useChat()
+
+const message = ref('')
+const topicsPanel = ref()
+const topicsOpen = ref(true)
+const isLarge = useMediaQuery('(min-width: 640px)')
+
+onClickOutside(topicsPanel, (event) => !isLarge.value ? topicsOpen.value = false : null)
+
+const chatWindow = ref();
+
+onMounted(() => {
+  watch(messages, () => {
+    nextTick(() => {
+      chatWindow.value.scrollTo(0, chatWindow.value.scrollHeight);
+    });
+  });
+});
+
+
 </script>
 
 <template lang='pug'>
-.flex.flex-col.m-4.rounded-3xl.bg-light-800.shadow-md.max-w-55ch.mx-auto
-  .p-4 
-    .text-xl.font-bold Chat
-  .flex.flex-col.bg-dark-50.bg-opacity-80.p-4.gap-2.max-h-50vh.overflow-y-scroll.scroll-smooth(ref="chatWindow")
-    chat-message(
-      :style="{ order: Math.round(Number(message.timestamp) / 1000) - 1640995200 }"
-      v-for="(message, key) in messages" :key="key"
-      :author="message.author"
-      :timestamp="message.timestamp"
-      :text="message.text"
-      ) 
+.flex.flex-col.bg-light-800.shadow-md.mx-auto.w-full
+  .p-4.flex.flex-wrap.items-center
+    .text-xl.font-bold Chat 
+    .flex-1.ml-2 / {{ currentTopic }}
+    button.button(v-if="!isLarge" @click.stop.prevent="topicsOpen = !topicsOpen") Channels
+  .flex
+    transition(name="fade")
+      .flex.flex-col.bg-dark-300.gap-2.h-50vh.overflow-y-scroll.scroll-smooth.absolute.sm_static.z-20.w-220px.max-w-full.text-light-900(style="flex: 1 1 420px" v-if="isLarge || (topicsOpen && !isLarge)" ref="topicsPanel")
+        .text-xl.font-bold.bg-dark-50.p-2 Channels
+        .flex.flex-col.p-2.gap-1
+          .font-bold.bg-light-100.bg-opacity-30.rounded-xl.px-2.cursor-pointer(
+            v-for="(count, topic) in topics" :key="topic"
+            @click="currentTopic = topic; topicsOpen = false"
+            ) {{ topic }} {{ count }}
+    .flex.flex-col.bg-dark-50.bg-opacity-80.p-4.gap-2.h-50vh.overflow-y-scroll.scroll-smooth(ref="chatWindow" style="flex: 1 1 auto")
+      chat-message(
+        :style="{ order: Math.round(Number(message.timestamp) / 1000) - 1640995200 }"
+        v-for="(message, key) in messages" :key="key"
+        :author="message.author"
+        :timestamp="message.timestamp"
+        :text="message.text"
+        ) 
   .p-4.rounded-2xl.bg-light-900.flex.gap-2(v-if="user.pub")
     input.p-2.rounded-xl.bg-light-200(
       v-model="message" placeholder="Your message"
-      @keyup.enter="send()"
+      @keyup.enter="send(message); message = ''"
       )
-    button.button(@click="send()" v-if="user.pub")
+    button.button(@click="send(message); message = ''" v-if="user.pub")
       la-comment-dots.mx-2
   .p-4.flex.flex-col.items-center(v-else)
     button.button(@click="user.auth = true") Log in to post messages
--- composables/src/chat/useChat.js
+++ composables/src/chat/useChat.js
@@ -3,49 +3,55 @@
  * @module Chat
  */
 
-import { ref, reactive, watch, nextTick, onMounted } from "vue";
+import { ref, reactive, computed, watch, nextTick, onMounted } from "vue";
 import { useUser, ms, useGun, currentRoom } from "..";
 
 export function useChat() {
   const gun = useGun();
-  const chatDb = gun.user(currentRoom.pub).get("chat");
+  const currentTopic = ref("general");
+  const topics = reactive({
+    general: 0,
+    news: 10,
+    announcements: 30,
+  });
+
   const { user } = useUser();
-  const message = ref("");
 
-  function send() {
-    if (!message.value) return;
-    let now = Date.now();
-    chatDb
-      .get(`${now}@${user.pub}`)
-      .put(message.value, null, { opt: { cert: currentRoom.features.chat } });
-    message.value = "";
-  }
+  const chatDb = computed(() =>
+    gun.user(currentRoom.pub).get("chat/" + currentTopic.value)
+  );
 
-  const messages = reactive({});
-  const chatWindow = ref();
-
-  chatDb.map().on((text, k) => {
-    const timestamp = k.substring(0, 13);
-    const author = k.substring(14);
-    messages[k] = {
-      timestamp,
-      author,
-      text,
-    };
-  });
-
-  onMounted(() => {
-    watch(messages, () => {
-      nextTick(() => {
-        chatWindow.value.scrollTo(0, chatWindow.value.scrollHeight);
-      });
+  const messages = computed(() => {
+    const msgs = reactive({});
+    chatDb.value.map().on((text, k) => {
+      const timestamp = k.substring(0, 13);
+      const author = k.substring(14);
+      msgs[k] = {
+        timestamp,
+        author,
+        text,
+      };
     });
+    return msgs;
   });
+
+  function send(message) {
+    if (!message) return;
+    let now = Date.now();
+    chatDb.value
+      .get(`${now}@${user.pub}`)
+      .put(message, null, { opt: { cert: currentRoom.features.chat } });
+  }
+  gun
+    .user(currentRoom.pub)
+    .get("chat")
+    .map()
+    .on((d) => console.log(d));
 
   return {
     send,
-    message,
+    currentTopic,
+    topics,
     messages,
-    chatWindow,
   };
 }
