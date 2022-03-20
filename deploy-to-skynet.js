e1ff7f621ed96776a0f18a85372e346fd62353eb
davay <fond@fondfrukt.ru>
2022-03-20 18:38:38 +0300

    clean

--- deploy-to-skynet.js
+++ deploy-to-skynet.js
@@ -4,9 +4,8 @@ import 'dotenv/config'
 const client = new SkynetClient('https://siasky.net', { APIKey: process.env.SKYNET_API_KEY })
 
 const { privateKey, publicKey } = genKeyPairFromSeed(process.env.SECRET_SEED);
-const dataKey = 'Gun-Vue'
+const dataKey = 'Gun-Vue';
 
-sia://fANEdzdRRRqEO8nZckaXcMNMY_tp-cmDVDCPoNCo9Jn0Sw
 
 (async () => {
   const skylink = await client.uploadDirectory("./_dist");
