ac828c930c8d76da8a1fce5888367b7d6f936c7a
davay <fond@fondfrukt.ru>
2022-07-08 17:02:49 +0300

    fix: story logos and links

--- components/histoire.config.js
+++ components/histoire.config.js
@@ -4,17 +4,22 @@ export default defineConfig({
   setupFile: 'histoire.setup.js',
   outDir: '../app/public/components/',
   vite: {
+    server: {
+      fs: {
+        allow: '..'
+      }
+    },
     base: '/components/'
   },
   routerMode: 'hash',
   theme: {
     title: 'Gun-Vue components',
-    // logo: {
-    //   square: '../docs/public/png/components.png',
-    //   light: '../docs/public/png/components.png',
-    //   dark: '../docs/public/png/components.png'
-    // },
-    // logoHref: 'https://gun-vue.js.org',
+    logo: {
+      square: '/gun-vue-logo.svg',
+      light: '/components.svg',
+      dark: '/components.svg'
+    },
+    logoHref: '/',
     // favicon: '../docs/public/svg/gun-vue-logo.svg',
   },
 })
