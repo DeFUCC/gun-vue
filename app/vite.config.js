6c48a2a87c0ce53bc09da0a81d31c94abc55db2e
davay <fond@fondfrukt.ru>
2022-08-17 02:25:23 +0300

    tabs

--- app/vite.config.js
+++ app/vite.config.js
@@ -18,109 +18,109 @@ const dirname = path.dirname(filename);
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  clearScreen: false,
-  server: {
-    port: 3142,
-    strictPort: true,
-  },
-  envPrefix: ['VITE_', 'TAURI_'],
-  publicDir: "public",
-  plugins: [
-    vue(),
-    Pages({
-      dirs: "src/pages",
-      routeBlockLang: 'yaml',
-      onRoutesGenerated: routes => (generateSitemap({ routes, hostname: 'https://gun-vue.js.org' })),
-    }),
-    WindiCSS({
-      scan: {
-        dirs: ["src", "../components/src"],
-        include: ["index.md"],
-        exclude: ["**/examples/**/*", "/node_modules/"],
-        fileExtensions: ["vue", "ts", "md"],
-      },
-    }),
-    Icons({
-      /* options */
-    }),
-    Components({
-      dirs: ["src/components"],
-      extensions: ["vue"],
-      directoryAsNamespace: false,
-      globalNamespaces: ["global"],
-      include: [/\.vue$/, /\.vue\?vue/, /\.md$/],
-      exclude: [/node_modules/, /\.git/],
-      resolvers: [
-        IconsResolver({
-          componentPrefix: "",
-        }),
-      ],
-    }),
-    // VitePWA({
-    //   registerType: "autoUpdate",
-    //   workbox: {
-    //     maximumFileSizeToCacheInBytes: 4000000
-    //   },
-    //   manifest: {
-    //     name: "Gun-Vue",
-    //     short_name: "Gun-Vue",
-    //     description: "Peer-to-peer graph database UI components",
-    //     iconPath: "./gun-vue-logo.png",
-    //     theme_color: "#43b883",
-    //     display: "standalone",
-    //     homepage_url: "https://gun-vue.js.org",
-    //     skylink: "AQC7upIKykiM-nYJA6Ac-Q4PHRtYsHDVS1Ne-M2ELcZU2A",
-    //     orientation: "portrait",
-    //     providedBy: {
-    //       name: "defucc",
-    //       url: "https://defucc.me",
-    //     },
-    //     icons: [
-    //       {
-    //         src: "./gun-vue-logo.svg",
-    //         type: "image/svg+xml",
-    //         sizes: "512x512",
-    //       },
-    //       {
-    //         src: "./gun-vue-logo.png",
-    //         sizes: "192x192",
-    //         type: "image/png",
-    //         purpose: "any maskable",
-    //       },
-    //       {
-    //         src: "./gun-vue-logo.png",
-    //         sizes: "512x512",
-    //         type: "image/png",
-    //         purpose: "any maskable",
-    //       },
-    //     ],
-    //   },
-    // }),
-  ],
-  base: './',
-  build: {
-    outDir: "../_dist/",
-    target: "esnext",
-    minify: !process.env.TAURI_DEBUG ? 'esbuild' : false,
-    sourcemap: !!process.env.TAURI_DEBUG,
-    brotliSize: true,
-    assetsInlineLimit: 100000000,
-    chunkSizeWarningLimit: 100000000,
-    cssCodeSplit: false,
-    rollupOptions: {
-      inlineDynamicImports: true,
-      output: {
-        manualChunks: () => "everything.js",
-      },
-    },
-  },
-  optimizeDeps: {
-    include: ["vue", "vue-router", "@vueuse/core"],
-  },
-  resolve: {
-    alias: {
-      "#components": path.resolve(dirname, "../components/src"),
-      "#composables": path.resolve(dirname, "../composables/src"),
-    },
-  },
+	clearScreen: false,
+	server: {
+		port: 3142,
+		strictPort: true,
+	},
+	envPrefix: ['VITE_', 'TAURI_'],
+	publicDir: "public",
+	plugins: [
+		vue(),
+		Pages({
+			dirs: "src/pages",
+			routeBlockLang: 'yaml',
+			onRoutesGenerated: routes => (generateSitemap({ routes, hostname: 'https://gun-vue.js.org' })),
+		}),
+		WindiCSS({
+			scan: {
+				dirs: ["src", "../components/src"],
+				include: ["index.md"],
+				exclude: ["**/examples/**/*", "/node_modules/"],
+				fileExtensions: ["vue", "ts", "md"],
+			},
+		}),
+		Icons({
+			/* options */
+		}),
+		Components({
+			dirs: ["src/components"],
+			extensions: ["vue"],
+			directoryAsNamespace: false,
+			globalNamespaces: ["global"],
+			include: [/\.vue$/, /\.vue\?vue/, /\.md$/],
+			exclude: [/node_modules/, /\.git/],
+			resolvers: [
+				IconsResolver({
+					componentPrefix: "",
+				}),
+			],
+		}),
+		// VitePWA({
+		//   registerType: "autoUpdate",
+		//   workbox: {
+		//     maximumFileSizeToCacheInBytes: 4000000
+		//   },
+		//   manifest: {
+		//     name: "Gun-Vue",
+		//     short_name: "Gun-Vue",
+		//     description: "Peer-to-peer graph database UI components",
+		//     iconPath: "./gun-vue-logo.png",
+		//     theme_color: "#43b883",
+		//     display: "standalone",
+		//     homepage_url: "https://gun-vue.js.org",
+		//     skylink: "AQC7upIKykiM-nYJA6Ac-Q4PHRtYsHDVS1Ne-M2ELcZU2A",
+		//     orientation: "portrait",
+		//     providedBy: {
+		//       name: "defucc",
+		//       url: "https://defucc.me",
+		//     },
+		//     icons: [
+		//       {
+		//         src: "./gun-vue-logo.svg",
+		//         type: "image/svg+xml",
+		//         sizes: "512x512",
+		//       },
+		//       {
+		//         src: "./gun-vue-logo.png",
+		//         sizes: "192x192",
+		//         type: "image/png",
+		//         purpose: "any maskable",
+		//       },
+		//       {
+		//         src: "./gun-vue-logo.png",
+		//         sizes: "512x512",
+		//         type: "image/png",
+		//         purpose: "any maskable",
+		//       },
+		//     ],
+		//   },
+		// }),
+	],
+	base: './',
+	build: {
+		outDir: "../_dist/",
+		target: "esnext",
+		minify: !process.env.TAURI_DEBUG ? 'esbuild' : false,
+		sourcemap: !!process.env.TAURI_DEBUG,
+		brotliSize: true,
+		assetsInlineLimit: 100000000,
+		chunkSizeWarningLimit: 100000000,
+		cssCodeSplit: false,
+		rollupOptions: {
+			inlineDynamicImports: true,
+			output: {
+				manualChunks: () => "everything.js",
+			},
+		},
+	},
+	optimizeDeps: {
+		include: ["vue", "vue-router", "@vueuse/core"],
+	},
+	resolve: {
+		alias: {
+			"#components": path.resolve(dirname, "../components/src"),
+			"#composables": path.resolve(dirname, "../composables/src"),
+		},
+	},
 });
