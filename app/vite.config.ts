7dd4e78b6eae09955377651b2c62902ca0a0adb8
davay <davay@defucc.me>
2023-04-05 00:30:22 +0700

    hm

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -9,6 +9,7 @@ import Unocss from 'unocss/vite'
 import path from "path";
 import { fileURLToPath } from "url";
 
+//@ts-expect-error
 const filename = fileURLToPath(import.meta.url);
 const dirname = path.dirname(filename);
 
