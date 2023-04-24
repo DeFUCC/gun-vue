6827fa64303c8223aacf06c675621753f2ae02f0
davay <davay@defucc.me>
2023-04-25 01:11:47 +0700

    fix: old space

--- src/space/SpacePlane.vue
+++ src/space/SpacePlane.vue
@@ -1,14 +1,27 @@
 <script setup lang="ts">
 import { watch } from "vue";
 import { useDrag, usePinch } from "@vueuse/gesture";
-import { useDraw, useSpace, useUser, useColor, useRoom, selectedUser, safeHash } from "../composables";
+import {
+  useDraw,
+  useSpace,
+  useUser,
+  useColor,
+  useRoom,
+  selectedUser,
+  safeHash,
+} from "../composables";
 import { ref, reactive, onMounted, onBeforeUnmount } from "vue";
 import { useDebounceFn, useThrottleFn } from "@vueuse/core";
-import { SpaceArrow, SpaceGuest, UiLayer, AccountHome } from '../components'
 
 const props = defineProps({
-  pad: { type: Number, default: 50, },
-  coord: { type: String, default: "", },
+  pad: {
+    type: Number,
+    default: 50,
+  },
+  coord: {
+    type: String,
+    default: "",
+  },
 });
 const emit = defineEmits(["user", "enter", "leave", "chat", "update:coord"]);
 
@@ -75,102 +88,75 @@ onMounted(() => {
 onBeforeUnmount(() => {
   drauu.unmount();
 });
-
-
 </script>
 
-
+<!-- eslint-disable vue/no-v-html -->
 <template lang="pug">
 .flex.flex-col.items-center
-  .text-2xl.p-8.top-15vh.cursor-pointer.absolute.rounded-3xl.shadow-xl.border-4(
-    v-if="!space.joined && user.is", 
-    :style="{ borderColor: user.color }", 
-    @click="join();"
-    ) Click anywhere to join the space
+
+  .text-2xl.p-8.top-15vh.cursor-pointer.absolute.rounded-3xl.shadow-xl.border-4(v-if='!space.joined && user.is', :style='{ borderColor: user.color }', @click='join();')
+    | Click anywhere to join the
+    | space
 
   space-draw.z-500
 
-  svg.h-full.w-full.z-200.bg-dark-100.bg-opacity-5.cursor-pointer.touch-none(
-    ref="plane", 
-    xmlns="http://www.w3.org/2000/svg", 
-    version="1.1", 
-    baseProfile="full", 
-    font-family="Commissioner , sans-serif", 
-    text-anchor="middle", 
-    dominant-baseline="middle", 
-    :viewBox="`${-pad + pos[0] - width / 2} ${-pad + pos[1] - height / 2} ${width * zoom + 2 * pad} ${height * zoom + 2 * pad}`", 
-    @click="!user.is ? user.auth = true : null; "
-    )
+  svg.h-full.w-full.z-200.bg-dark-100.bg-opacity-5.cursor-pointer.touch-none(ref='plane', 
+  xmlns='http://www.w3.org/2000/svg', 
+  version='1.1', 
+  baseProfile='full', 
+  font-family='Commissioner , sans-serif', 
+  text-anchor='middle', 
+  dominant-baseline='middle', 
+  :viewBox=' `${-pad + pos[0] - width / 2} ${-pad + pos[1] - height / 2} ${width * zoom + 2 * pad} ${height * zoom + 2 * pad}` ',
+  @click=' !user.is ? user.auth = true : null; ')
 
     defs
-      filter#shadowButton(x="-50%", height="200%", width="300%")
-        feDropShadow(dx="0", dy="3", stdDeviation="3", flood-color="#2225")
-      clipPath#mask(clipPathUnits="objectBoundingBox")
-        circle(r=".5", cx=".5", cy=".5")
+      filter#shadowButton(x='-50%', height='200%', width='300%')
+        feDropShadow(dx='0', dy='3', stdDeviation='3', flood-color='#2225')
+      clipPath#mask(clipPathUnits='objectBoundingBox')
+        circle(r='.5', cx='.5', cy='.5')
+
+    text.text-xs(
+      text-anchor='end', 
+      :transform=' `translate(${pos[0] + width / 2 - 10} ${pos[1] - height / 2 + 20})` '
+      ) {{ pos }}
 
     g.opacity-90(
-      v-for="guest in guests", :key="guest.draw", 
-      v-html="guest.draw")
+      v-for='  guest   in   guests  ', :key=' guest.draw ', v-html=' guest.draw ')
 
-    g.frame(
-      :transform="`translate(${pos[0] - width / 2 -pad} ${pos[1] - height / 2 -pad})`"
-      )
-      text.text-xs(
-        text-anchor="end", 
-          :x="width-10"
-          :y="20"
-        ) {{ pos }}
+    svg.opacity-70(
+      ref='paper', 
+      :x=' pos[0] - width / 2 - pad ', 
+      :y=' pos[1] - height / 2 - pad ', 
+      :viewBox=' `${-pad + pos[0] - width / 2} ${-pad + pos[1] - height / 2} ${width + 2 * pad} ${height + 2 * pad}` ', 
+      :class=" { 'pointer-events-none': !draw.enabled, 'touch-none': draw.enabled } ")
 
-      rect(
-        ref="area", :x="pad" :y="pad"
-        rx="12", :width="width", :height="height", 
-        fill="none", stroke="#3333", stroke-width="1")
+    rect(ref='area', 
+    :x=' pos[0] - width / 2 ', 
+    :y=' pos[1] - height / 2 ', 
+    rx='12', 
+    :width=' width ', 
+    :height=' height ', 
+    fill='none', stroke='#3333', stroke-width='1')
 
-      svg.opacity-70(
-        ref="paper", 
-        :x="0", 
-        :y="0", 
-        :width="width"
-        :height="height"
-        :viewBox="`${ pos[0] - width / 2} ${ pos[1] - height / 2} ${width + 2 * pad} ${height + 2 * pad}`", 
-        :class="{ 'pointer-events-none': !draw.enabled, 'touch-none': draw.enabled }")
+    g.link
+      line(
+      :x1=' pos[0] '
+      :x2=' space.my.mouse.x ',
+      :y1=' pos[1] ',
+      :y2=' space.my.mouse.y ', 
+      :stroke=' user.color ', stroke-dasharray='6')
 
-    g.user
-      g.link
-        line(
-          :x1="pos[0]", 
-          :x2="space.my.mouse.x", 
-          :y1="pos[1]", 
-          :y2="space.my.mouse.y", 
-          :stroke="user.color", 
-          stroke-dasharray="6")
+    g.pointer(:transform=' `translate(${pos[0]} ${pos[1]})` ')
+      g.mouse
+        circle(style='filter:url(#shadowButton)', :fill=' user.color ', r='8')
 
-      g.pointer(:transform="`translate(${pos[0]} ${pos[1]})`")
-        g.mouse
-          circle(style="filter:url(#shadowButton)", :fill="user.color", r="8")
+    g.arrows
+      space-arrow(v-for='(  link, key  ) in   links  ', :key=' key ', :link=' link ', @user=' selectedUser.pub = $event ')
 
-    g.users
-      g.arrows
-        space-arrow(v-for="(link, key) in links", :key="key", :link="link", @user="selectedUser.pub = $event")
+    g.guests
+      space-guest.cursor-pointer.transition-all.ease-out.duration-600(v-for='  guest   in   guests  ', :key=' guest ', v-bind=' guest ', :style=' { transform: `translate(${guest?.pos?.x}px, ${guest?.pos?.y}px)` } ', @click=' selectedUser.pub = guest.pub ')
 
-      g.guests
-        SpaceGuest.cursor-pointer.transition-all.ease-out.duration-600(
-          v-for="guest in guests", 
-          :key="guest.pub", 
-          v-bind="guest", 
-          :style="{ transform: `translate(${guest?.pos?.x}px, ${guest?.pos?.y}px)` }", 
-          @click="selectedUser.pub = guest.pub")
-
-  ui-layer.z-4000(
-    :open="!!selectedUser.pub", 
-    @close="selectedUser.pub = null"
-    )
-    account-home.max-w-600px(
-      :key="selectedUser.pub", 
-      :pub="selectedUser.pub", 
-      @user="$emit('user', $event)", 
-      @post="$emit('post', safeHash($event))", 
-      @chat="$emit('chat', selectedUser.pub)", 
-      @close="selectedUser.pub = null"
-      )
+  ui-layer.z-4000(:open=' !!selectedUser.pub ', @close=' selectedUser.pub = null ')
+    account-home.max-w-600px(:key=' selectedUser.pub ', :pub=' selectedUser.pub ', @user=" $emit('user', $event) ", @post=" $emit('post', String(safeHash($event))) ", @chat=" $emit('chat', selectedUser.pub) ", @close=' selectedUser.pub = null ')
 </template>
