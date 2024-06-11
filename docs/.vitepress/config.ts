7938b438adce48567c88b8e9bf089e2388cd3584
davay <davay@defucc.me>
2024-06-11 11:33:29 +0700

    bump deps

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -1,41 +1,42 @@
 import { defineConfig, DefaultTheme } from "vitepress";
 
-import { sidebar } from './navigation'
-import path from 'path'
-import pack from '../package.json'
+import { sidebar } from "./navigation";
+import path from "path";
+import pack from "../package.json";
 
 export default defineConfig({
+	ignoreDeadLinks: true,
 	lang: "en-US",
 	title: "Gun-Vue",
-	titleTemplate: 'Gun-Vue docs',
-	description: 'Reactive p2p crypto graph app toolkit',
+	titleTemplate: "Gun-Vue docs",
+	description: "Reactive p2p crypto graph app toolkit",
 	base: "/docs/",
 	outDir: "../app/public/docs",
 	themeConfig: {
-		logo: 'gun-vue-logo.svg',
+		logo: "gun-vue-logo.svg",
 		search: {
-			provider: 'local'
+			provider: "local",
 		},
 		sidebar,
 		socialLinks: [
-			{ icon: 'github', link: 'https://github.com/DeFUCC/gun-vue' },
-			{ icon: 'youtube', link: 'https://www.youtube.com/@defucc' },
+			{ icon: "github", link: "https://github.com/DeFUCC/gun-vue" },
+			{ icon: "youtube", link: "https://www.youtube.com/@defucc" },
 		],
 		footer: {
-			message: 'MIT',
+			message: "MIT",
 			copyright: `v.${pack.version}`,
 		},
 	},
 	async transformPageData(pageData, { siteConfig }) {
-		const pages = siteConfig?.pages.map(p => {
+		const pages = siteConfig?.pages.map((p) => {
 			// let id = path.join(siteConfig.site.base, p)
-			let id = p
-			id = id.replace(/\/index\.md$/, '/')
-			id = id.replace(/\.md$/, '')
-			return id
-		})
+			let id = p;
+			id = id.replace(/\/index\.md$/, "/");
+			id = id.replace(/\.md$/, "");
+			return id;
+		});
 		return {
-			pages
-		}
-	}
+			pages,
+		};
+	},
 });
--- src/room/useRoom.ts
+++ src/room/useRoom.ts
@@ -1,164 +1,173 @@
 /**
+ * [[include:./room/README.md]]
  * @module Room
  * @group Rooms
  */
 
+/**
+
+ * @module useProject
+ * @group Projects
+ */
 import {
-  useGun,
-  generateCerts,
-  useUser,
-  user,
-  hashText,
-  downloadFile,
-  relay
+	useGun,
+	generateCerts,
+	useUser,
+	user,
+	hashText,
+	downloadFile,
+	relay,
 } from "../composables";
 import config from "../../gun.config.json";
 import { reactive, computed, ref, watchEffect } from "vue";
 import { ISEAPair } from "gun";
-import { useStorage } from '@vueuse/core'
+import { useStorage } from "@vueuse/core";
 import { CurrentRoom } from "./useRoom.d";
 
-const rootRoom = config.room
+const rootRoom = config.room;
 
 export const currentRoom: CurrentRoom = reactive({
-  pub: useStorage('current-room', rootRoom.pub),
-  isRoot: computed(() => currentRoom.pub == rootRoom.pub),
-  hosts: {},
-  features: {},
-  profile: {},
+	pub: useStorage("current-room", rootRoom.pub),
+	isRoot: computed(() => currentRoom.pub == rootRoom.pub),
+	hosts: {},
+	features: {},
+	profile: {},
 });
 
 watchEffect(() => {
-  const gun = useGun();
+	const gun = useGun();
 
-  if (currentRoom.pub == rootRoom.pub) {
-    currentRoom.hosts = rootRoom.hosts;
-    currentRoom.features = rootRoom.features;
-  } else {
-    currentRoom.features = {};
-    currentRoom.hosts = {};
-    gun
-      .user(currentRoom.pub)
-      .get("hosts")
-      .map()
-      .once((d, k) => {
-
-        currentRoom.hosts[k] = d;
-      });
-    gun
-      .user(currentRoom.pub)
-      .get("features")
-      .map()
-      .once((d, k) => {
-
-        currentRoom.features[k] = d;
-      });
-  }
+	if (currentRoom.pub == rootRoom.pub) {
+		currentRoom.hosts = rootRoom.hosts;
+		currentRoom.features = rootRoom.features;
+	} else {
+		currentRoom.features = {};
+		currentRoom.hosts = {};
+		gun
+			.user(currentRoom.pub)
+			.get("hosts")
+			.map()
+			.once((d, k) => {
+				currentRoom.hosts[k] = d;
+			});
+		gun
+			.user(currentRoom.pub)
+			.get("features")
+			.map()
+			.once((d, k) => {
+				currentRoom.features[k] = d;
+			});
+	}
 });
-
 
 /**
  * Reactive room controls
  */
 
 export function useRoom(pub = currentRoom.pub) {
+	const room: CurrentRoom = reactive({
+		pub: pub,
+		isRoot: computed(() => pub == rootRoom.pub),
+		hosts: {},
+		features: {},
+		profile: {},
+	});
 
-  const room: CurrentRoom = reactive({
-    pub: pub,
-    isRoot: computed(() => pub == rootRoom.pub),
-    hosts: {},
-    features: {},
-    profile: {},
-  });
+	const gun = useGun();
 
-  const gun = useGun();
+	gun
+		.user(pub)
+		.get("profile")
+		.map()
+		.on((d, k) => {
+			room.profile[k] = d;
+		});
+	gun
+		.user(pub)
+		.get("hosts")
+		.map()
+		.once((d, k) => {
+			room.hosts[k] = d;
+		});
+	gun
+		.user(pub)
+		.get("features")
+		.map()
+		.once((d, k) => {
+			room.features[k] = d;
+		});
 
-  gun
-    .user(pub)
-    .get("profile")
-    .map()
-    .on((d, k) => {
-      room.profile[k] = d;
-    });
-  gun
-    .user(pub)
-    .get("hosts")
-    .map()
-    .once((d, k) => {
-      room.hosts[k] = d;
-    });
-  gun
-    .user(pub)
-    .get("features")
-    .map()
-    .once((d, k) => {
-      room.features[k] = d;
-    });
-
-  return {
-    room,
-    submitRoom,
-    enterRoom,
-    createRoom,
-    leaveRoom,
-    updateRoomProfile,
-  };
+	return {
+		room,
+		submitRoom,
+		enterRoom,
+		createRoom,
+		leaveRoom,
+		updateRoomProfile,
+	};
 }
 
 export function useRoomLogo(pub = currentRoom.pub) {
+	const logo = ref();
+	const gun = useGun();
+	gun
+		.user(pub)
+		.get("profile")
+		.get("logo")
+		.on((hash) => {
+			if (!hash) {
+				logo.value = null;
+				return;
+			}
+			gun
+				.get("#logos")
+				.get(hash)
+				.once((d) => {
+					logo.value = d;
+				});
+		});
 
-  const logo = ref()
-  const gun = useGun();
-  gun.user(pub).get('profile').get('logo').on(hash => {
-    if (!hash) {
-      logo.value = null
-      return
-    }
-    gun.get('#logos').get(hash).once(d => {
-      logo.value = d
-    })
-  })
+	async function uploadLogo(file: string) {
+		if (file) {
+			const hash = await hashText(file);
+			gun.get("#logos").get(hash).put(file);
+			updateRoomProfile("logo", hash);
+		} else {
+			removeLogo();
+		}
+	}
 
-  async function uploadLogo(file: string) {
-    if (file) {
-      const hash = await hashText(file)
-      gun.get('#logos').get(hash).put(file)
-      updateRoomProfile('logo', hash)
-    } else {
-      removeLogo()
-    }
+	function removeLogo() {
+		updateRoomProfile("logo", null);
+	}
 
-  }
-
-  function removeLogo() {
-    updateRoomProfile('logo', null)
-  }
-
-  return {
-    logo, uploadLogo, removeLogo
-  }
+	return {
+		logo,
+		uploadLogo,
+		removeLogo,
+	};
 }
 
 export function useRooms() {
-  const rooms = computed(() => {
-    return listPersonal("rooms", currentRoom.pub);
-  });
-  return { rooms, createRoom };
+	const rooms = computed(() => {
+		return listPersonal("rooms", currentRoom.pub);
+	});
+	return { rooms, createRoom };
 }
 
 export function listPersonal(tag: string, pub = currentRoom.pub) {
-  const gun = useGun();
-  const records = reactive({});
-  gun
-    .user(pub)
-    .get(`${tag}`)
-    .map()
-    .on(function (data, key) {
-      let k = key.substring(0, 87);
-      records[k] = records[k] || {};
-      records[k][key.substring(88)] = data;
-    });
-  return records;
+	const gun = useGun();
+	const records = reactive({});
+	gun
+		.user(pub)
+		.get(`${tag}`)
+		.map()
+		.on(function (data, key) {
+			let k = key.substring(0, 87);
+			records[k] = records[k] || {};
+			records[k][key.substring(88)] = data;
+		});
+	return records;
 }
 
 /**
@@ -168,123 +177,151 @@ export function listPersonal(tag: string, pub = currentRoom.pub) {
  */
 
 export function updateRoomProfile(field: string, content: any) {
-  const gun = useGun();
-  const { user } = useUser();
-  let certificate = currentRoom.hosts?.[user.pub]?.profile;
-  gun
-    .user(currentRoom.pub)
-    .get("profile")
-    .get(field)
-    .put(content, null, { opt: { cert: certificate } });
+	const gun = useGun();
+	const { user } = useUser();
+	let certificate = currentRoom.hosts?.[user.pub]?.profile;
+	gun
+		.user(currentRoom.pub)
+		.get("profile")
+		.get(field)
+		.put(content, null, { opt: { cert: certificate } });
 }
 
 /**
  * Create a new room inside the current room
  */
 
-export async function createRoom({ pair, name }: { pair: ISEAPair, name?: string }) {
-  const { user } = useUser();
-  // if (!pair) pair = await SEA.pair();
-  if (!pair) return;
-  const roomPub = pair.pub
+export async function createRoom({
+	pair,
+	name,
+}: {
+	pair: ISEAPair;
+	name?: string;
+}) {
+	const { user } = useUser();
+	// if (!pair) pair = await SEA.pair();
+	if (!pair) return;
+	const roomPub = pair.pub;
 
-  const certs = await generateCerts({
-    pair,
-    list: [
-      { tag: "profile", users: [user.pub] },
-      { tag: "features", users: [user.pub] },
-      { tag: "hosts", users: [user.pub] },
-    ],
-  });
-  const features = await generateCerts({
-    pair,
-    list: Object.keys(config?.features).map(f => ({ tag: f, personal: true })),
-  });
+	const certs = await generateCerts({
+		pair,
+		list: [
+			{ tag: "profile", users: [user.pub] },
+			{ tag: "features", users: [user.pub] },
+			{ tag: "hosts", users: [user.pub] },
+		],
+	});
+	const features = await generateCerts({
+		pair,
+		list: Object.keys(config?.features).map((f) => ({
+			tag: f,
+			personal: true,
+		})),
+	});
 
-  const enc = await user.encrypt(`${pair}`);
+	const enc = await user.encrypt(`${pair}`);
 
-  const gunConfig = {
-    ...config,
-    relay: relay.peer,
-    room: {
-      pub: pair.pub,
-      hosts: { [user.pub]: { enc, ...certs } },
-      features,
-    }
-  }
+	const gunConfig = {
+		...config,
+		relay: relay.peer,
+		room: {
+			pub: pair.pub,
+			hosts: { [user.pub]: { enc, ...certs } },
+			features,
+		},
+	};
 
-  console.log(
-    "COPY THIS ROOM INFO TO USE IT AS A ROOT",
-    gunConfig,
-    "STORE THIS KEY PAIR IN A SAFE PLACE",
-    pair
-  );
+	console.log(
+		"COPY THIS ROOM INFO TO USE IT AS A ROOT",
+		gunConfig,
+		"STORE THIS KEY PAIR IN A SAFE PLACE",
+		pair
+	);
 
-  const gun = useGun();
-  await gun.user().get("safe").get("rooms").get(roomPub).put(enc).then();
+	const gun = useGun();
+	await gun.user().get("safe").get("rooms").get(roomPub).put(enc).then();
 
-  await gun.user(roomPub)
-    .get("hosts")
-    .get(user.pub)
-    .put(
-      {
-        enc,
-        ...certs,
-      },
-      null,
-      { opt: { cert: certs.hosts } }
-    ).then();
+	await gun
+		.user(roomPub)
+		.get("hosts")
+		.get(user.pub)
+		.put(
+			{
+				enc,
+				...certs,
+			},
+			null,
+			{ opt: { cert: certs.hosts } }
+		)
+		.then();
 
-  await gun.user(roomPub).get("features").put(features, null, { opt: { cert: certs.features } }).then();
+	await gun
+		.user(roomPub)
+		.get("features")
+		.put(features, null, { opt: { cert: certs.features } })
+		.then();
 
-  if (name) {
-    await gun.user(roomPub).get("profile").put({ name }, null, { opt: { cert: certs.profile } }).then();
-  }
+	if (name) {
+		await gun
+			.user(roomPub)
+			.get("profile")
+			.put({ name }, null, { opt: { cert: certs.profile } })
+			.then();
+	}
 
-  await gun
-    .user(currentRoom.pub)
-    .get("rooms")
-    .get(`${roomPub}@${user.pub}`)
-    .put(true, null, { opt: { cert: currentRoom?.features?.rooms } }).then();
+	await gun
+		.user(currentRoom.pub)
+		.get("rooms")
+		.get(`${roomPub}@${user.pub}`)
+		.put(true, null, { opt: { cert: currentRoom?.features?.rooms } })
+		.then();
 
-  downloadFile(JSON.stringify(gunConfig), 'application/json', 'gun.config.json')
-  downloadFile(JSON.stringify(pair), 'application/json', `room_${name || roomPub}.json`)
-  // enterRoom(pair.pub);
+	downloadFile(
+		JSON.stringify(gunConfig),
+		"application/json",
+		"gun.config.json"
+	);
+	downloadFile(
+		JSON.stringify(pair),
+		"application/json",
+		`room_${name || roomPub}.json`
+	);
+	// enterRoom(pair.pub);
 }
 
 export async function recreateRoom(enc: string, name?: string) {
-  //@ts-ignore
-  const dec: ISEAPair = await user.decrypt(enc);
-  createRoom({
-    pair: dec,
-    name
-  });
+	//@ts-ignore
+	const dec: ISEAPair = await user.decrypt(enc);
+	createRoom({
+		pair: dec,
+		name,
+	});
 }
 
 export async function submitRoom(pub: string) {
-  const gun = useGun();
-  const already = await gun
-    .user(currentRoom.pub)
-    .get("rooms")
-    .get(`${pub}@${user.pub}`)
-    .then();
+	const gun = useGun();
+	const already = await gun
+		.user(currentRoom.pub)
+		.get("rooms")
+		.get(`${pub}@${user.pub}`)
+		.then();
 
-  gun
-    .user(currentRoom.pub)
-    .get("rooms")
-    .get(`${pub}@${user.pub}`)
-    .put(!already, null, { opt: { cert: currentRoom.features?.rooms } });
+	gun
+		.user(currentRoom.pub)
+		.get("rooms")
+		.get(`${pub}@${user.pub}`)
+		.put(!already, null, { opt: { cert: currentRoom.features?.rooms } });
 }
 
 export function joinRoom() {
-  const gun = useGun();
-  gun
-    .user(currentRoom.pub)
-    .get("space")
-    .get(user.pub)
-    .put(JSON.stringify({ x: Math.random(), y: Math.random() }), null, {
-      opt: { cert: currentRoom.features?.space },
-    });
+	const gun = useGun();
+	gun
+		.user(currentRoom.pub)
+		.get("space")
+		.get(user.pub)
+		.put(JSON.stringify({ x: Math.random(), y: Math.random() }), null, {
+			opt: { cert: currentRoom.features?.space },
+		});
 }
 
 /**
@@ -293,7 +330,7 @@ export function joinRoom() {
  */
 
 export function enterRoom(pub: string): void {
-  currentRoom.pub = pub;
+	currentRoom.pub = pub;
 }
 
 /**
@@ -301,35 +338,35 @@ export function enterRoom(pub: string): void {
  */
 
 export function leaveRoom() {
-  currentRoom.pub = rootRoom.pub;
+	currentRoom.pub = rootRoom.pub;
 }
 
 export async function addPersonal({
-  tag,
-  key,
-  text,
-  pub = currentRoom.pub,
-  cert,
+	tag,
+	key,
+	text,
+	pub = currentRoom.pub,
+	cert,
 }: {
-  tag: string
-  key: string
-  text: string
-  pub: string
-  cert: string
+	tag: string;
+	key: string;
+	text: string;
+	pub: string;
+	cert: string;
 }) {
-  const gun = useGun();
-  if (!cert) cert = await gun.user(pub).get("features").get(tag).then();
-  if (!cert) {
-    cert = currentRoom.features?.[`${tag}`];
-  }
-  if (!cert && pub != user.pub) {
-    console.log("No certificate found");
-    return;
-  }
+	const gun = useGun();
+	if (!cert) cert = await gun.user(pub).get("features").get(tag).then();
+	if (!cert) {
+		cert = currentRoom.features?.[`${tag}`];
+	}
+	if (!cert && pub != user.pub) {
+		console.log("No certificate found");
+		return;
+	}
 
-  gun
-    .user(pub)
-    .get(`${tag}`)
-    .get(`${key}@${user.pub}`)
-    .put(text, null, { opt: { cert } });
+	gun
+		.user(pub)
+		.get(`${tag}`)
+		.get(`${key}@${user.pub}`)
+		.put(text, null, { opt: { cert } });
 }
