bd040550d40551ec32f9bd585757b18b1324f024
davay <davay@defucc.me>
2024-07-12 13:49:32 +0700

    bumps

--- src/crypto/useCrypto.ts
+++ src/crypto/useCrypto.ts
@@ -10,7 +10,7 @@ import { ISEAPair } from "gun";
 import { SEA } from "../gun/composables";
 
 export function isHash(str: string): boolean {
-  return typeof str == 'string' && str.length == 44 && str.charAt(43) == '='
+	return typeof str == "string" && str.length == 44 && str.charAt(43) == "=";
 }
 
 /**
@@ -20,8 +20,8 @@ export function isHash(str: string): boolean {
  */
 
 export interface Entity {
-  pub: string
-  epub: string
+	pub: string;
+	epub: string;
 }
 
 /**
@@ -33,10 +33,14 @@ export interface Entity {
  * @param {Entity} receiver - An object with `pub` and `epub` strings - the user.is object of the reciever's account
  * @returns {Promise<string>} Encrypted data string to be sent
  */
-export async function encFor(data: string, sender: ISEAPair, receiver: Entity): Promise<string> {
-  const secret = await SEA.secret(receiver.epub, sender);
-  const encryptedData = await SEA.encrypt(data, secret);
-  return encryptedData;
+export async function encFor(
+	data: string,
+	sender: ISEAPair,
+	receiver: Entity
+): Promise<string> {
+	const secret = await SEA.secret(receiver.epub, sender);
+	const encryptedData = await SEA.encrypt(data, secret);
+	return encryptedData;
 }
 
 /**
@@ -48,80 +52,94 @@ export async function encFor(data: string, sender: ISEAPair, receiver: Entity): 
  * @param {ISEAPair} receiver - SEA Pair of the receiver – `epriv` key will be used to encrypt the data
  * @returns {Promise<string>} Decrypted data
  */
-export async function decFrom(data: string, sender: Entity, receiver: ISEAPair): Promise<string> {
-  const secret = await SEA.secret(sender.epub, receiver);
-  const decryptedData = await SEA.decrypt(data, secret);
-  return decryptedData;
+export async function decFrom(
+	data: string,
+	sender: Entity,
+	receiver: ISEAPair
+): Promise<string> {
+	const secret = await SEA.secret(sender.epub, receiver);
+	const decryptedData = await SEA.decrypt(data, secret);
+	return decryptedData;
 }
 
 export async function hashText(text: string): Promise<string> {
-  let hash = await SEA.work(text, null, null, { name: "SHA-256" });
-  return hash;
+	let hash = await SEA.work(text, null, null, { name: "SHA-256" });
+	return hash;
 }
 
-export async function hashObj(obj: object): Promise<{ hash: string; hashed: string; }> {
-  let hashed = typeof obj == "string" ? obj : JSON.stringify(obj);
-  let hash = await hashText(hashed);
-  return { hash, hashed };
+export async function hashObj(
+	obj: object
+): Promise<{ hash: string; hashed: string }> {
+	let hashed = typeof obj == "string" ? obj : JSON.stringify(obj);
+	let hash = await hashText(hashed);
+	return { hash, hashed };
 }
 
 /**
  * Calculate a hex hash for any string data
  * @async
  * @param {String} text
- * @param {String} seed
+ * @param {String} salt
  * @returns {String} The hex encoded SHA-1 hash
  */
-export async function getShortHash(text: string, salt: string): Promise<string> {
-  return await SEA.work(text, null, null, { name: "PBKDF2", encode: "hex", salt });
+export async function getShortHash(
+	text: string,
+	salt: string
+): Promise<string> {
+	return await SEA.work(text, null, null, {
+		name: "PBKDF2",
+		encode: "hex",
+		salt,
+	});
 }
 
 // Buffer -> Base64 String -> Url Safe Base64 String
 export function safeHash(unsafe: string): string {
-  if (!unsafe) return;
-  const encode_regex = /[+=/]/g;
-  return unsafe.replace(encode_regex, encodeChar);
+	if (!unsafe) return;
+	const encode_regex = /[+=/]/g;
+	return unsafe.replace(encode_regex, encodeChar);
 }
 
 function encodeChar(c: string): string {
-  switch (c) {
-    case "+":
-      return "-";
-    case "=":
-      return ".";
-    case "/":
-      return "_";
-  }
+	switch (c) {
+		case "+":
+			return "-";
+		case "=":
+			return ".";
+		case "/":
+			return "_";
+	}
 }
 
 // Url Safe Base64 String -> Base64 String -> Buffer
 export function unsafeHash(safe: string): string {
-  if (!safe) return;
-  const decode_regex = /[._-]/g;
-  return safe.replace(decode_regex, decodeChar);
+	if (!safe) return;
+	const decode_regex = /[._-]/g;
+	return safe.replace(decode_regex, decodeChar);
 }
 
 function decodeChar(c: string): string {
-  switch (c) {
-    case "-":
-      return "+";
-    case ".":
-      return "=";
-    case "_":
-      return "/";
-  }
+	switch (c) {
+		case "-":
+			return "+";
+		case ".":
+			return "=";
+		case "_":
+			return "/";
+	}
 }
 
 export function safeJSONParse(input: string | object, def?: object): object {
-  // Convert null to empty object
-  if (!input) {
-    return def || {};
-  } else if (typeof input == 'object') { //Object.prototype.toString.call(input) === "[object Object]"
-    return input;
-  }
-  try {
-    return JSON.parse(input);
-  } catch (e) {
-    return def || {};
-  }
+	// Convert null to empty object
+	if (!input) {
+		return def || {};
+	} else if (typeof input == "object") {
+		//Object.prototype.toString.call(input) === "[object Object]"
+		return input;
+	}
+	try {
+		return JSON.parse(input);
+	} catch (e) {
+		return def || {};
+	}
 }
