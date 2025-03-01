25084b52292c291679e5c41c97af4fbc1c5f6bf8
davay <davay@defucc.me>
2025-03-01 15:19:25 +0700

    feat: better PWA, SEO and social links

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -8,7 +8,7 @@ export default defineConfig({
 	lang: "en-US",
 	title: "Gun-Vue",
 	titleTemplate: "Gun-Vue docs",
-	description: "Reactive p2p crypto graph app toolkit",
+	description: "The Peer-to-Peer Web App Toolkit",
 	base: "/",
 	outDir: "../_dist",
 	themeConfig: {
@@ -26,6 +26,9 @@ export default defineConfig({
 			copyright: `v.${pack.version}`,
 		},
 	},
+	head: [
+		['meta', { name: 'keywords', content: 'p2p, peer-to-peer, decentralized, vue, vue3, gun, gundb, e2ee, end-to-end encryption, crypto, web3, dapp, pwa, offline-first, real-time, reactive, composables, components, webrtc, webtorrent, social, chat, file sharing' }]
+	],
 	transformHead({ pageData }) {
 		return [
 			process.env.NODE_ENV === "production" && ["script", { async: true, defer: true, "data-website-id": "63481594-170a-40b4-b3b3-7b62799d0dd2", src: "https://stat.defucc.me/script.js" }],
